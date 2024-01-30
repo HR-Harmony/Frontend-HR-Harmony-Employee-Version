c5e0cce336f511b67f8a7a84cfb2857c0296a976
Arfara Yema S <indonesianstudent@gmail.com>
2024-01-30 22:00:13 +0700

    slicing calendar

--- src/Components/Tasks/TasksCalendar.jsx
+++ src/Components/Tasks/TasksCalendar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './TasksCalendar.css';
+
+const localizer = momentLocalizer(moment);
 
 const TasksCalendar = () => {
-  return (
-    <div>TasksCalendar</div>
-  )
-}
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-export default TasksCalendar
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const events = [
+    {
+      title: 'Meeting 1',
+      start: new Date(2024, 0, 15, 10, 0),
+      end: new Date(2024, 0, 15, 12, 0),
+    },
+    {
+      title: 'Meeting 2',
+      start: new Date(2024, 0, 20, 14, 0),
+      end: new Date(2024, 0, 20, 16, 0),
+    },
+  ];
+
+  return (
+    <div className='calendar-container'>
+      <div className='date-picker-container'>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="dd/MM/yyyy"
+        />
+      </div>
+      <div className='calendar'>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          date={selectedDate}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TasksCalendar;
