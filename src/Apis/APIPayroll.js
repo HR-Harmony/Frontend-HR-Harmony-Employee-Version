a10bd922bf71cfb437077b1802f7e3057db70fa7
Fakhrity Hikmawan <fakhrityhikmawan@gmail.com>
2024-04-20 16:35:38 +0700

    Consume API Payroll #2

--- src/Apis/APIPayroll.js
+++ src/Apis/APIPayroll.js
@@ -27,5 +27,148 @@ export const APIPayroll = {
     } catch (error) {
       throw new Error(error);
     }
+  },
+
+  updatePayrollStatus: async (payrollId, paidStatus) => {
+    try {
+      const result = await axiosInstance.put(`/payrolls/${payrollId}`, { paid_status: paidStatus }, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  createAdvanceSalary: async (advanceSalaryData) => {
+    try {
+      const result = await axiosInstance.post('/advance_salaries', advanceSalaryData, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  getAllAdvanceSalaries: async () => {
+    try {
+      const result = await axiosInstance.get('/advance_salaries', {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  getAdvanceSalaryById: async (id) => {
+    try {
+      const result = await axiosInstance.get(`/advance_salaries/${id}`, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  updateAdvanceSalaryById: async (id, advanceSalaryData) => {
+    try {
+      const result = await axiosInstance.put(`/advance_salaries/${id}`, advanceSalaryData, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  deleteAdvanceSalaryById: async (id) => {
+    try {
+      const result = await axiosInstance.delete(`/advance_salaries/${id}`, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  createRequestLoan: async (requestLoanData) => {
+    try {
+      const result = await axiosInstance.post('/request_loans', requestLoanData, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  getAllRequestLoans: async () => {
+    try {
+      const result = await axiosInstance.get('/request_loans', {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  getRequestLoanById: async (id) => {
+    try {
+      const result = await axiosInstance.get(`/request_loans/${id}`, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  updateRequestLoanById: async (id, requestLoanData) => {
+    try {
+      const result = await axiosInstance.put(`/request_loans/${id}`, requestLoanData, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  deleteRequestLoanById: async (id) => {
+    try {
+      const result = await axiosInstance.delete(`/request_loans/${id}`, {
+        headers: {
+          'Authorization': `Bearer YOUR_TOKEN_HERE`
+        }
+      });
+      return result.data;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 };
